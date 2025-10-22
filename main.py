895e4f8753d8dca6134ba895ed982843ab4b0a4f
Younes Bensafia <younes.bensafia@outlook.com>
2025-10-22 01:31:24 +0100

    Refactor main function in exo1 to return an integer; add print_game_reduction function for game output

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from src.randomMatrix import random_matrix as matrix
+from src.exercises.exo1.main import main as exo1
 
 
 def main() -> None:
-    print(matrix(3, 3)[1])
-
-    
+    exo1()
 
 if __name__ == "__main__":
     main()
--- src/exercises/exo1/main.py
+++ src/exercises/exo1/main.py
@@ -2,11 +2,17 @@ import copy
 from utils.exo1.dominance_algorithm import remove_dominated_strategies
 from config.exo1_config import GAME 
 
-def main() -> None:
+def main() -> int:
+    return print_game_reduction()
+
+def print_game_reduction():
     game_copy = copy.deepcopy(GAME)
     reduced, _, _ = remove_dominated_strategies(game_copy)
+    print("original game:")
+    print(GAME)
+    print("reduced game:")
     print(reduced)
+    return 0
     
-
 if __name__ == "__main__":
     main()
